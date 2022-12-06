b457fac8fe226fc5e161b05fe75d8372568313f7
EZ <testrailsemail@gmail.com>
2022-12-06 16:16:07 +0200

    update app to use middleware and auth routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,32 @@
-require('dotenv').config();
-import express from 'express';
-import config from 'config';
-import { Prisma, PrismaClient } from '@prisma/client'
+import cors from "cors";
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import morgan from "morgan";
+import { env } from "./env/server.mjs";
+import { router as authRouter } from "@api/auth/auth.routes.js";
 
 const app = express();
-const prisma = new PrismaClient()
+// MIDDLEWARE
 
-const port = config.get<number>('port');
+// 1.Body Parser
+app.use(express.json({ limit: '10kb' }));
+
+// 2. Cors
+app.use(
+  cors({
+    origin: 'http://localhost:8000',
+    credentials: true,
+  })
+);
+
+// 3. Logger
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// ROUTES
+app.use('/api/auth', authRouter);
+
+const port = env.PORT;
 app.listen(port, () => {
   console.log(`🚀 Server started on port: ${port}`);
 });
