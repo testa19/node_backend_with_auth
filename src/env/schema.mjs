c6f4e783e4e37f57dc20fa7b3ba02c9bfc98b184
EZ <testrailsemail@gmail.com>
2022-12-06 16:08:45 +0200

    type validate env vars

---
+++ src/env/schema.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { z } from "zod";
+
+/**
+ * Specify your server-side environment variables schema here.
+ * This way you can ensure the app isn't built with invalid env vars.
+ */
+export const serverSchema = z.object({
+  DATABASE_URL: z.string().url(),
+  NODE_ENV: z.enum(["development", "test", "production"]),
+  PORT: z.string().min(4).max(5),
+  ACCESS_TOKEN_PRIVATE_KEY: z.string(),
+  ACCESS_TOKEN_PUBLIC_KEY: z.string(),
+  JWT_ACCESS_SECRET: z.string(),
+  JWT_REFRESH_SECRET: z.string(),
+});
