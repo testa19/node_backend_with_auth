19459df7bd5e202ea21c3a313de094c08e02689f
EZ <testrailsemail@gmail.com>
2022-12-06 18:16:45 +0200

    add global error handler and unhandled routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import cors from "cors";
 import * as dotenv from "dotenv";
 dotenv.config();
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 import { env } from "./env/server.mjs";
 import { router as authRouter } from "@api/auth/auth.routes.js";
+import AppError from "./utils/appError.js";
 
 const app = express();
 // MIDDLEWARE
@@ -25,6 +26,22 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 // ROUTES
 app.use('/api/auth', authRouter);
+
+// UNHANDLED ROUTES
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  next(new AppError(404, `Route ${req.originalUrl} not found`));
+});
+
+// GLOBAL ERROR HANDLER
+app.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 const port = env.PORT;
 app.listen(port, () => {
