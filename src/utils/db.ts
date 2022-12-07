f81fd29e9b7ad921e1f475c02c961e08695f32f4
EZ <testrailsemail@gmail.com>
2022-12-07 13:59:53 +0200

    extend prisma query logging

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { env } from "~/env/server.mjs";
 
 declare global {
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient<Prisma.PrismaClientOptions, 'info' | 'query' | 'warn' | 'error'> | undefined;
 }
 
 export const prisma =
@@ -11,9 +11,28 @@ export const prisma =
   new PrismaClient({
     log:
       env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
+        ? [
+            {
+              emit: "event",
+              level: "query",
+            },
+            {
+              emit: "stdout",
+              level: "error",
+            },
+            {
+              emit: "stdout",
+              level: "warn",
+            },
+          ]
         : ["error"],
   });
+
+prisma.$on("query", (e: Prisma.QueryEvent) => {
+  console.log("Query: " + '\u001b[' + 33 + 'm' + e.query + '\u001b[0m');
+  console.log("Params: " + e.params);
+  console.log('\u001b[' + 32 + 'm' + "Duration: " + e.duration + "ms" + '\u001b[0m');
+});
 
 if (env.NODE_ENV !== "production") {
   global.prisma = prisma;
